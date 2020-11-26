1c976b2f5416ca1017920a3fc0151ed0024efc5c
Nikita <63471940+Daelon02@users.noreply.github.com>
2020-11-26 08:40:52 +0200

    Update file path

---
+++ js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.problems').css('opacity', '1');
+  }, 1000);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.problems').css('opacity', '0.001');
+  }, 5000);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.install').css('opacity', '1');
+  }, 3000);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.install').css('opacity', '0.001');
+  }, 6000);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.animphone').css('opacity', '1');
+  }, 8500);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.join-comp').css('opacity', '1');
+  }, 10000);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.text-jc').css('opacity', '1');
+  }, 11500);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.text-jc').css('opacity', '1');
+  }, 13000);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.subs').css('opacity', '1');
+  }, 14500);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.costsub').css('opacity', '1');
+  }, 16000);
+});
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.cost').css('opacity', '1');
+  }, 17500);
+});
